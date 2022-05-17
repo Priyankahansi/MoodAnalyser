1c2ff56a0f232db2bcf480d86efecfb8d4cc66ed
Priyanka <priyankahansi95@gmail.com>
2022-05-17 15:56:50 +0530

    added:welcome message

---
+++ src/main/java/com/bridgelabz/MainClass.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MoodAnalyser mood=new MoodAnalyser();
+        mood.analyzeMood("happy");
+    }
+}
