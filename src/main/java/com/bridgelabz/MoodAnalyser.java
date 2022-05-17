4633d27a02856f3bb06d95c66965f8a37f6e6948
Priyanka <priyankahansi95@gmail.com>
2022-05-17 18:31:58 +0530

    added:ExceptionHandle(InvalidMood)

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -2,32 +2,16 @@ package com.bridgelabz;
 
 public class MoodAnalyser {
     String message;
-    public static String analyzeMood(String message) {
-        if (message.contains("sad")) {
-            return "SAD";
-        } else if (message.contains("Happy")) {
+
+    public String analyseMood(String message) {
+        try {
+            if (message.contains("Sad")) {
+                return "SAD";
+            }
+        } catch (NullPointerException e) {
             return "HAPPY";
-        } else {
-            return null;
         }
-    }
-    public static String analyzeMood2(String message) {
-        if (message.contains("i am in sad mood")) {
-            return "SAD";
-        } else if (message.contains("Happy")) {
-            return "HAPPY";
-        } else {
-            return null;
-        }
-    }
-    public static String analyzeMood3(String message) {
-        if (message.contains("i am in any mood")) {
-            return "Happy";
-        } else if (message.contains("i am in sad mood")) {
-            return "sad";
-        } else {
-            return null;
-        }
+        return null;
     }
 }
 
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -7,25 +7,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoodAnalyserTest extends MoodAnalyser {
-    @Test
-    void givenMessageWhenSadShouldReturnSad() {
-        MoodAnalyser mood = new MoodAnalyser();
-        String result = MoodAnalyser.analyzeMood("sad");
-        assertEquals("SAD", result);
-    }
+
 
     @Test
-    void givenMessageWhenIaminsadmoodShouldReturnSad() {
-        MoodAnalyser mood = new MoodAnalyser();
-        String result = MoodAnalyser.analyzeMood2("i am in sad mood");
-        assertEquals("SAD", result);
-
-    }
-
-    void givenMessageWhenIaminsadmoodShouldReturnHappy() {
-        MoodAnalyser mood = new MoodAnalyser();
-        String result = MoodAnalyser.analyzeMood3("i am in any mood");
-        assertEquals("SAD", result);
+    public void givenNullMood_ShouldThrowException() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("Sad");
+        assertEquals("SAD", mood);
     }
 }
 
